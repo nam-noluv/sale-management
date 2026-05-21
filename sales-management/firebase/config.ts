538b82dad40f175c8f952cc60fb0414c8d2a3318
Tran Nam <trannam11092004@gmail.com>
2026-05-21 16:11:24 +0700

    fix home page

--- sales-management/firebase/config.ts
+++ sales-management/firebase/config.ts
@@ -1,19 +1,37 @@
-
 import { initializeApp } from "firebase/app";
+
 import { getFirestore } from "firebase/firestore";
+
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBEY6bEGqAhhplKR5ID5RwJzOInqJcL27g",
-  authDomain: "sale-69e7b.firebaseapp.com",
-  projectId: "sale-69e7b",
-  storageBucket: "sale-69e7b.firebasestorage.app",
-  messagingSenderId: "293552266841",
-  appId: "1:293552266841:web:0714f264ba2e4ea68a9b48",
-  measurementId: "G-VN1TRZG2BV"
+  apiKey:
+    process.env
+      .NEXT_PUBLIC_FIREBASE_API_KEY,
+
+  authDomain:
+    process.env
+      .NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+
+  projectId:
+    process.env
+      .NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+
+  storageBucket:
+    process.env
+      .NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+
+  messagingSenderId:
+    process.env
+      .NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+
+  appId:
+    process.env
+      .NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
+export  const db = getFirestore(app);
+
 export const auth = getAuth(app);
